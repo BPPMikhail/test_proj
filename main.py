0423d981be04f0a2983435600a02d56c070d6a54
DontTouchMyMind <tobigface@gmail.com>
2020-09-03 10:08:45 +0200

    #1 Created class Game that allows the player to select the number of attempts.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+class Game:
+
+    def __init__(self, allowed_attempts: int):
+        self.__allowed_attempts = allowed_attempts
+
+difficult_games = int(input('Enter the number of available attempts: '))
+
+game = Game(difficult_games)
